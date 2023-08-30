20cfa5a1e59cf8751731021e6870fe2349473209
liuyihua <529732278@qq.com>
2023-08-30 21:25:28 +0800

    01_小程序升级代码

---
+++ 01_hello_world/App.vue
@@ -0,0 +1,20 @@
+<script>
+	import checkUpdate from '@/uni_modules/uni-upgrade-center-app/utils/check-update.js'
+
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+			checkUpdate()
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
